a46697a7dc74275b7b78a1608f9bab2c98a577a9
Juan31452 <jcs1011@gmail.com>
2022-09-03 22:23:26 -0500

    se modifica controlador

--- ProyectoCiclo3/src/main/java/mintic/grupo61/CovidCol/controlador/Controlador.java
+++ ProyectoCiclo3/src/main/java/mintic/grupo61/CovidCol/controlador/Controlador.java
@@ -1,22 +1,85 @@
 package mintic.grupo61.CovidCol.controlador;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
+import java.util.List;
+import java.util.Optional;
+import mintic.grupo61.CovidCol.modelo.usuario.UsuarioServicio;
+import mintic.grupo61.CovidCol.modelo.usuario.Usuarios;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
-@Controller
-@Slf4j
+//Controlador que manipula el flujo de los servicios rest del microservicio de animes.
+
+@RestController
+//@Controller
+//@Slf4j
+@RequestMapping("/vacunas")
 public class Controlador {
     
-    @GetMapping("/")
-   
-    public String inicio()
-    {
-        log.info("Desde Controlador");
-        //log.debug("Mas Informacion");
-        return "index";
-    }        
-    
+    @Autowired
+    private UsuarioServicio usuarioservicioImpl;
+        //listar
+	@GetMapping
+	@RequestMapping(value = "consultarusuarios", method = RequestMethod.GET)
+	public ResponseEntity<?> consultarUsuarios() {
+		
+		List<Usuarios> usuariosConsultados = this.usuarioservicioImpl.consultarUsuarios();
+		
+		return ResponseEntity.ok(usuariosConsultados);
+	}
+        //guardar
+        @PostMapping
+        @RequestMapping(value = "guardarusuarios", method = RequestMethod.POST)
+	public ResponseEntity<?> guardarUsuario(@RequestBody Usuarios usuarios)
+        {
+            Usuarios Usuarioguardado = this.usuarioservicioImpl.guardarUsuario(usuarios);
+            return ResponseEntity.status(HttpStatus.CREATED).body(Usuarioguardado);
+        }
+        
+        //actualizar
+        @PutMapping
+        @RequestMapping(value = "actualizarusuarios", method = RequestMethod.PUT)
+	public ResponseEntity<?> actualizarUsuario(@RequestBody Usuarios usuarios)
+        {
+            Usuarios Usuarioactualizado = this.usuarioservicioImpl.actualizarUsuario(usuarios);
+            return ResponseEntity.status(HttpStatus.CREATED).body(Usuarioactualizado);
+        }
+        
+        //eliminar
+        @DeleteMapping
+        @RequestMapping(value = "eliminarusuarios/{Idusuario}", method = RequestMethod.DELETE)
+	public ResponseEntity<?> eliminarUsuario(@PathVariable Long Idusuario)
+        {
+            this.usuarioservicioImpl.eliminarUsuario(Idusuario);
+            return ResponseEntity.ok().build();
+        }
+
+        //buscar
+        @GetMapping
+        @RequestMapping(value = "buscarusuario/{Idusuario}", method = RequestMethod.GET)
+	public ResponseEntity<?> buscarid(@PathVariable Long Idusuario)
+        {
+             Usuarios usuarioconsulta = this.usuarioservicioImpl.buscarid(Idusuario);
+            return ResponseEntity.ok(usuarioconsulta);
+        }
+        
+        //buscar por npasaporte
+        @GetMapping
+        @RequestMapping(value = "buscarpasaporte/{npasaporte}", method = RequestMethod.GET)
+	public ResponseEntity<?> buscarporpasaporte(@PathVariable String npasaporte)
+        {
+             Optional<Usuarios> consultapasaporte = this.usuarioservicioImpl.buscarporpasaporte(npasaporte);
+            return ResponseEntity.ok(consultapasaporte);
+        }
+
+
 }
