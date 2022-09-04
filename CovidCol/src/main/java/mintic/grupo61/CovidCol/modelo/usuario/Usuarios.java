0932687b8fe08d1b79a168cc87cceb720cd7789e
Juan31452 <jcs1011@gmail.com>
2022-09-03 19:38:17 -0500

    se agrega entidad usuarios y conexion a bd

--- CovidCol/src/main/java/mintic/grupo61/CovidCol/modelo/usuario/Usuarios.java
+++ CovidCol/src/main/java/mintic/grupo61/CovidCol/modelo/usuario/Usuarios.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.Data;
 
-@Data
+@Data //se encarga de los getters y setters
 @Entity
 @Table(name = "usuarios")
 public class Usuarios implements Serializable 
@@ -31,71 +31,6 @@ public class Usuarios implements Serializable
   private String nacionalidad;
   @Column
   private String eps;
-  
-    //Constructor
-    public Usuarios() {
-        
-    }
+
  
-    public String getNpasaporte() {
-        return npasaporte;
-    }
-
-    public Long getIdusuario() {
-        return Idusuario;
-    }
-
-    public void setIdusuario(Long Idusuario) {
-        this.Idusuario = Idusuario;
-    }
-
-    public void setNpasaporte(String npasaporte) {
-        this.npasaporte = npasaporte;
-    }
-
-    public String getCorreo() {
-        return correo;
-    }
-
-    public void setCorreo(String correo) {
-        this.correo = correo;
-    }
-
-    public String getContraseña() {
-        return contraseña;
-    }
-
-    public void setContraseña(String contraseña) {
-        this.contraseña = contraseña;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
-    public String getNacionalidad() {
-        return nacionalidad;
-    }
-
-    public void setNacionalidad(String nacionalidad) {
-        this.nacionalidad = nacionalidad;
-    }
-
-    public String getEps() {
-        return eps;
-    }
-
-    public void setEps(String eps) {
-        this.eps = eps;
-    }
-  
-    @Override
-    public String toString() 
-    {
-        return String.format("%-15s %-20s %-10s %-12s %-12s %-12s ", npasaporte, correo,contraseña,telefono,nacionalidad,eps );    
-    }      
 }
