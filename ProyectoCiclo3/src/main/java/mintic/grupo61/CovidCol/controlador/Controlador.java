0f74026541f0bd406a8da7a03668eb301cde22ff
Juan31452 <jcs1011@gmail.com>
2022-09-04 11:23:37 -0500

    se modifica controlador y agrega usuarios.html

--- ProyectoCiclo3/src/main/java/mintic/grupo61/CovidCol/controlador/Controlador.java
+++ ProyectoCiclo3/src/main/java/mintic/grupo61/CovidCol/controlador/Controlador.java
@@ -2,84 +2,76 @@ package mintic.grupo61.CovidCol.controlador;
 
 import java.util.List;
 import java.util.Optional;
-import mintic.grupo61.CovidCol.modelo.usuario.UsuarioServicio;
-import mintic.grupo61.CovidCol.modelo.usuario.Usuarios;
+import lombok.extern.slf4j.Slf4j;
+import mintic.grupo61.CovidCol.modelo.usuario.UsuarioInterface;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-//Controlador que manipula el flujo de los servicios rest del microservicio de animes.
+//Controlador que manipula el flujo de los servicios rest del microservicio de Covidcol.
 
-@RestController
-//@Controller
-//@Slf4j
-@RequestMapping("/vacunas")
+@Controller
+@Slf4j
+@RequestMapping("/vacunas")   // http:localhost:8080/vacunas
 public class Controlador {
     
     @Autowired
-    private UsuarioServicio usuarioservicioImpl;
+    private UsuarioInterface usuarioInterface;
         //listar
-	@GetMapping
-	@RequestMapping(value = "consultarusuarios", method = RequestMethod.GET)
-	public ResponseEntity<?> consultarUsuarios() {
-		
-		List<Usuarios> usuariosConsultados = this.usuarioservicioImpl.consultarUsuarios();
-		
-		return ResponseEntity.ok(usuariosConsultados);
-	}
-        //guardar
-        @PostMapping
-        @RequestMapping(value = "guardarusuarios", method = RequestMethod.POST)
-	public ResponseEntity<?> guardarUsuario(@RequestBody Usuarios usuarios)
+	@GetMapping("")
+        //@RequestMapping(value = "consultarusuarios", method = RequestMethod.GET)
+	public String listar(Model modelo)
         {
-            Usuarios Usuarioguardado = this.usuarioservicioImpl.guardarUsuario(usuarios);
-            return ResponseEntity.status(HttpStatus.CREATED).body(Usuarioguardado);
+            modelo.addAttribute("lista", usuarioInterface.findAll());
+            return "usuarios";
         }
+        // //guardar
+        // @PostMapping
+        // @RequestMapping(value = "guardarusuarios", method = RequestMethod.POST)
+	// public ResponseEntity<?> guardarUsuario(@RequestBody Usuarios usuarios)
+        // {
+        //     Usuarios Usuarioguardado = this.usuarioservicioImpl.guardarUsuario(usuarios);
+        //     return ResponseEntity.status(HttpStatus.CREATED).body(Usuarioguardado);
+        // }
         
-        //actualizar
-        @PutMapping
-        @RequestMapping(value = "actualizarusuarios", method = RequestMethod.PUT)
-	public ResponseEntity<?> actualizarUsuario(@RequestBody Usuarios usuarios)
-        {
-            Usuarios Usuarioactualizado = this.usuarioservicioImpl.actualizarUsuario(usuarios);
-            return ResponseEntity.status(HttpStatus.CREATED).body(Usuarioactualizado);
-        }
+        // //actualizar
+        // @PutMapping
+        // @RequestMapping(value = "actualizarusuarios", method = RequestMethod.PUT)
+	// public ResponseEntity<?> actualizarUsuario(@RequestBody Usuarios usuarios)
+        // {
+        //     Usuarios Usuarioactualizado = this.usuarioservicioImpl.actualizarUsuario(usuarios);
+        //     return ResponseEntity.status(HttpStatus.CREATED).body(Usuarioactualizado);
+        // }
         
-        //eliminar
-        @DeleteMapping
-        @RequestMapping(value = "eliminarusuarios/{Idusuario}", method = RequestMethod.DELETE)
-	public ResponseEntity<?> eliminarUsuario(@PathVariable Long Idusuario)
-        {
-            this.usuarioservicioImpl.eliminarUsuario(Idusuario);
-            return ResponseEntity.ok().build();
-        }
+        // //eliminar
+        // @DeleteMapping
+        // @RequestMapping(value = "eliminarusuarios/{Idusuario}", method = RequestMethod.DELETE)
+	// public ResponseEntity<?> eliminarUsuario(@PathVariable Long Idusuario)
+        // {
+        //     this.usuarioservicioImpl.eliminarUsuario(Idusuario);
+        //     return ResponseEntity.ok().build();
+        // }
 
-        //buscar
-        @GetMapping
-        @RequestMapping(value = "buscarusuario/{Idusuario}", method = RequestMethod.GET)
-	public ResponseEntity<?> buscarid(@PathVariable Long Idusuario)
-        {
-             Usuarios usuarioconsulta = this.usuarioservicioImpl.buscarid(Idusuario);
-            return ResponseEntity.ok(usuarioconsulta);
-        }
+        // //buscar
+        // @GetMapping
+        // @RequestMapping(value = "buscarusuario/{Idusuario}", method = RequestMethod.GET)
+	// public ResponseEntity<?> buscarid(@PathVariable Long Idusuario)
+        // {
+        //      Usuarios usuarioconsulta = this.usuarioservicioImpl.buscarid(Idusuario);
+        //     return ResponseEntity.ok(usuarioconsulta);
+        // }
         
-        //buscar por npasaporte
-        @GetMapping
-        @RequestMapping(value = "buscarpasaporte/{npasaporte}", method = RequestMethod.GET)
-	public ResponseEntity<?> buscarporpasaporte(@PathVariable String npasaporte)
-        {
-             Optional<Usuarios> consultapasaporte = this.usuarioservicioImpl.buscarporpasaporte(npasaporte);
-            return ResponseEntity.ok(consultapasaporte);
-        }
+        // //buscar por npasaporte
+        // @GetMapping
+        // @RequestMapping(value = "buscarpasaporte/{npasaporte}", method = RequestMethod.GET)
+	// public ResponseEntity<?> buscarporpasaporte(@PathVariable String npasaporte)
+        // {
+        //      Optional<Usuarios> consultapasaporte = this.usuarioservicioImpl.buscarporpasaporte(npasaporte);
+        //     return ResponseEntity.ok(consultapasaporte);
+        // }
 
 
 }
