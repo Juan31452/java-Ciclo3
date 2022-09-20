b158eb92a92fe9bdc998f448e6e95abee52f0515
Juan Carlos <jcs1011@gmail.com>
2022-09-20 16:15:01 -0500

    se actualiza script_BD.sql

--- ProyectoCiclo3/script_BD.sql
+++ ProyectoCiclo3/script_BD.sql
@@ -8,12 +8,20 @@ USE Vacunas;
 #Tablas
 CREATE TABLE IF NOT EXISTS usuarios(
 Idusuario INT AUTO_INCREMENT,
+Nombres VARCHAR(20) NOT NULL,
+Apellidos VARCHAR(20) NOT NULL,
+Pais_nacimiento VARCHAR(15) NOT NULL,
+Fecha_Nacimiento DATE NOT NULL,
+Dpto_Residencia VARCHAR(12) NOT NULL,
+Ciudad_Residencia VARCHAR(12) NOT NULL,
+Direccion VARCHAR(15) NOT NULL,
+Tipo_Documento VARCHAR(3) NOT NULL,
+Numero_Documento VARCHAR(20) NOT NULL,
 Npasaporte VARCHAR(15) NOT NULL,
+Fecha_VencePasaporte DATE NOT NULL,
 Correo VARCHAR (20) NOT NULL,
 Contraseña VARCHAR(10) NOT NULL,
-Telefono VARCHAR (12),
-Nacionalidad VARCHAR(12),
-Eps VARCHAR (12),
+Confirmar_Contraseña VARCHAR(10) NOT NULL
 PRIMARY KEY(Idusuario)
 )ENGINE = INNODB;
 
@@ -22,8 +30,8 @@ CREATE TABLE IF NOT EXISTS ciudad(
 Id_ciudad INT AUTO_INCREMENT,
 Ciudad_Actual VARCHAR(15) NOT NULL,
 Ciudad_destino VARCHAR(15) NOT NULL,
-Numero_vacunas INT,
-Sintomas INT,
+Numero_vacunas INT NOT NULL,
+Sintomas INT NOT NULL,
 Idusuario INT NOT NULL,
 PRIMARY KEY (Id_ciudad),
 FOREIGN KEY (Idusuario)
