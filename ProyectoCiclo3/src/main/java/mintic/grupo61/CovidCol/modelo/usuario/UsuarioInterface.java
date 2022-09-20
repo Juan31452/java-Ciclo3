0218b9767f833bdf2580aa986c65e86c77efe53e
Juan Carlos <jcs1011@gmail.com>
2022-09-20 17:26:20 -0500

    se corrige conexion mysql tabla ciudad

--- ProyectoCiclo3/src/main/java/mintic/grupo61/CovidCol/modelo/usuario/UsuarioInterface.java
+++ ProyectoCiclo3/src/main/java/mintic/grupo61/CovidCol/modelo/usuario/UsuarioInterface.java
@@ -21,7 +21,7 @@ public interface UsuarioInterface extends CrudRepository<Usuarios, Long>
   //List<Usuarios> findBycorreoAndcontraseña(String correo, String contraseña);
   @Query (value = "select u.Idusuario,u.nombres,u.apellidos,u.pais_nacimiento,u.fecha_nacimiento,u.dpto_residencia,u.ciudad_residencia,"
           + "u.direccion,u.tipo_documento,u.numero_documento,u.npasaporte,u.fecha_vencepasaporte,u.correo,u.contraseña,u.confirmar_contraseña"
-          + "  from Usuarios u where u.correo =:micorreo and contraseña=:micontraseña", nativeQuery = true)
+          + "  from usuarios u where u.correo =:micorreo and contraseña=:micontraseña", nativeQuery = true)
   public List<Usuarios> consultausuario(@Param("micorreo")  String correo,@Param("micontraseña") String contraseña);
   
 }
