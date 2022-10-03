b81a000d0a0a5b25f905921599583a6e53707700
Juan Carlos <jcs1011@gmail.com>
2022-10-03 15:37:50 -0500

    se agrega campo fecha

--- ProyectoCiclo3/src/main/java/mintic/grupo61/CovidCol/modelo/ciudad/Ciudad.java
+++ ProyectoCiclo3/src/main/java/mintic/grupo61/CovidCol/modelo/ciudad/Ciudad.java
@@ -1,19 +1,15 @@
 package mintic.grupo61.CovidCol.modelo.ciudad;
 
 import java.io.Serializable;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.Vector;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import mintic.grupo61.CovidCol.modelo.usuario.Usuarios;
@@ -27,7 +23,11 @@ public class Ciudad implements Serializable {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @EqualsAndHashCode.Include()
   //@Column(name = "idciudad")
-  private Long Idciudad;
+  private Long Idciudad;  
+  //@Temporal(TemporalType.DATE)
+  //@DateTimeFormat(pattern = "dd-MM-AAAA") 
+  @Column(name = "fecha")
+  private String fecha;  
   @Column(name = "ciudad_actual", nullable = false, length = 30)
   private String ciudad_actual;
   @Column(name = "ciudad_destino", nullable = false, length = 30)
